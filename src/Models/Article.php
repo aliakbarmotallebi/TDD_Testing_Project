b1a082e16210a08bdac49e75d9055c0cd27ac2b7
motallebi.aliakbar <motallebi.aliakbar@gmail.com>
2022-05-13 17:37:33 +0430

    test for article entity create generetor slug

--- src/Models/Article.php
+++ src/Models/Article.php
@@ -1,6 +1,18 @@
 <?php namespace App\Models;
 
+use phpDocumentor\Reflection\Types\Null_;
 
 class Article {
 
+  public $title;
+
+  public function getSlug()
+  {
+		$text  = $this->title;
+    $text  = strtolower($text);
+		$text  = preg_replace( '/[^A-Za-z0-9 _.]/', '', $text );
+		$text  = preg_replace( '/[ _.]+/', '-', trim( $text ) );
+		$text  = trim( $text, '-' );
+    return $text;
+  }
 }
--- tests/Models/ArticleTest.php
+++ tests/Models/ArticleTest.php
@@ -5,9 +5,41 @@ use App\Models\Article;
 
 class ArticleTest extends TestCase {
 
+  protected $article;
+
+  public function setUp(): void
+  {
+    $this->article = (new Article);
+  }
+
   public function testTitleIsEmptyByDefault()
   {
-    $article = new Article;
-    $this->assertEmpty($article->title);
+    $this->assertEmpty($this->article->title);
   }
+
+  public function testSlugIsEmptyWithNoTitle()
+  {
+    $this->assertSame($this->article->getSlug(), "");
+  }
+
+  public function titleProvider()
+  {
+    return [
+       "string simple and strtolower" => ["Mens Cotton Jacket", "mens-cotton-jacket"],
+       "trim the string" => ["Mens Casual Premium Slim Fit T-Shirts ", "mens-casual-premium-slim-fit-tshirts"],
+       "only take alphanumerical characters" => ["Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops", "fjallraven-foldsack-no-1-backpack-fits-15-laptops"],
+       "replace spaces by dashes" => ["SanDisk SSD PLUS 1TB Internal SSD - SATA III 6 Gb/s", "sandisk-ssd-plus-1tb-internal-ssd-sata-iii-6-gbs"]
+    ];
+  }
+
+  /**
+   * @dataProvider titleProvider
+   */
+  public function testSlug($title, $slug)
+  {
+    $this->article->title = $title;
+
+    $this->assertEquals($this->article->getSlug(), $slug);
+  }
+
 }
